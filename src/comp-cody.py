d2b53849b503bc941d893aaa578fb8845aeafabb
Sai SriHarsha Griddaluru <sriharsha.9554@gmail.com>
2022-12-17 15:09:17 +0530

    algo-worlflow-neovim-tried

--- src/comp-cody.py
+++ src/comp-cody.py
@@ -1,30 +1,37 @@
 import sys
+
 sys.stdin = open("./io/in.txt", "r")
 sys.stdout = open("./io/out.txt", "wt")
+
 
 def gen_bin_helper(i, s, n, bin):
     if i == n:
         bin.append(s)
     else:
-        gen_bin_helper(i+1, s+'0', n, bin)
-        gen_bin_helper(i+1, s+'1', n, bin)
+        gen_bin_helper(i + 1, s + "0", n, bin)
+        gen_bin_helper(i + 1, s + "1", n, bin)
+
 
 def gen_bin(n):
     bin = []
     gen_bin_helper(0, "", n, bin)
     return bin
 
-def square_it(n):
-    return n**2
-
-def cube_it(n):
-    return n**3
 
 def main():
     n = int(input())
+    items = list(map(str, input().split()))
+    res = []
     bin = gen_bin(n)
     for b in bin:
-        print(b)
+        # b = '1000'
+        temp = []
+        for i in range(n):
+            if b[i] == "1":
+                temp.append(items[i])
+        res.append(temp)
+    for item in res:
+        print(item)
 
 
 main()
